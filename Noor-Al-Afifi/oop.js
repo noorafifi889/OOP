c15bb39c756bfcd2a3c04b67ed10a411209c4b31
noorafifi889 <noorelafifi889@gmail.com>
2025-08-21 10:06:54 +0300

    Student System with Constructor Functions & Encapsulation

---
+++ Noor-Al-Afifi/oop.js
@@ -0,0 +1,37 @@
+// Second Task: (Student System)
+// 1. Create a Student constructor function with the following properties:
+//     - name
+//     - age
+//     - grade
+//     - introduce() method that logs a message like:
+
+function Student(name, age) {
+  this.name = name;
+  this.age = age;
+
+  const grade = 0; // private proparity
+
+    this.setGrade = function (grade) { // setter method
+        if (grade == 'A' || grade == 'F') {
+            this.grade = grade;
+        }
+        else {
+            console.log("Grade must be A or F.");
+        }
+    }
+  this.getGrade= function() {  
+    return this.grade;
+  }
+  this.introduce = function () {
+   console.log(
+       `Hi, my name is ${this.name}, I’m  ${
+           this.age
+       } years old and I’m in grade ${this.getGrade()}`
+   );
+  };
+}
+
+const student = new Student("Noor", 20);
+student.setGrade('F');
+student.introduce()
+
